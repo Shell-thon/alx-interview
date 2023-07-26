98b464654003e30073b021ba27b472f40691d514
Shell-thon <shellterminal0@gmail.com>
2023-07-26 08:49:12 +0100

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,21 @@
-#!/usr/bin/python3 
- """ 
- 0-pascal_triangle 
- """ 
-  
-  
- def pascal_triangle(n): 
-     """ 
-     Returns a list of integers 
-     representing the Pascal Triangle of n 
-     returns empty list if n <= 0 
-     """ 
-     k = [] 
-     if n <= 0: 
-         return k 
-     k = [[1]] 
-     for i in range(1, n): 
-         temp = [1] 
-         for j in range(len(k[i - 1]) - 1): 
-             curr = k[i - 1] 
-             temp.append(k[i - 1][j] + k[i - 1][j + 1]) 
-         temp.append(1) 
-         k.append(temp) 
-     return k
+def pascal_triangle(n): 
+    """ 
+    Returns a list of lists of integers representing the Pascal Triangle of n 
+    Returns an empty list if n <= 0 
+    """ 
+    if n <= 0: 
+        return [] 
+    triangle = [[1]] 
+    for i in range(1, n): 
+        temp = [1] 
+        for j in range(len(triangle[i - 1]) - 1): 
+            temp.append(triangle[i - 1][j] + triangle[i - 1][j + 1]) 
+        temp.append(1) 
+        triangle.append(temp) 
+    return triangle
+
+# Call the function and print the result
+if __name__ == "__main__":
+    result = pascal_triangle(5)
+    for row in result:
+        print("[{}]".format(",".join([str(x) for x in row])))
