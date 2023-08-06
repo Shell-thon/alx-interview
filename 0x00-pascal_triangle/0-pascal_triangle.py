de950db320902bf19f2810bee81c6cd8826cf786
Shell-thon <shellterminal0@gmail.com>
2023-08-06 08:20:48 -0700

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,23 @@
-#!/usr/bin/python3 
- """ 
- 0-pascal_triangle 
- """
+#!/usr/bin/python3
+"""
+contains the Pascal's Triangle function
+"""
 
 
-def pascal_triangle(n): 
-    """ 
-    Returns a list of lists of integers representing the Pascal Triangle of n 
-    Returns an empty list if n <= 0 
-    """ 
-    if n <= 0: 
-        return [] 
-    triangle = [[1]] 
-    for i in range(1, n): 
-        temp = [1] 
-        for j in range(len(triangle[i - 1]) - 1): 
-            temp.append(triangle[i - 1][j] + triangle[i - 1][j + 1]) 
-        temp.append(1) 
-        triangle.append(temp) 
+def pascal_triangle(n):
+    """ returns a list of lists of integers representing
+        the Pascal’s triangle of n
+    """
+    triangle = []
+    for i in range(1, n+1):
+        row = []
+        for j in range(i):
+            if j == 0 or j == i-1:
+                n = 1
+                row.append(n)
+            else:
+                n = triangle[i-2][j-1] + triangle[i-2][j]
+                row.append(n)
+        triangle.append(row)
+
     return triangle
-
-# Call the function and print the result
-if __name__ == "__main__":
-    result = pascal_triangle(5)
-    for row in result:
-        print("[{}]".format(",".join([str(x) for x in row])))
--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-Curriculum <br> 
- **Short Specializations** <br> 
-# Alx Interview 
-  
-![Interview](https://github.com/Shell-thon/alx-interview/assets/113628692/5b7a822e-aa65-4a4d-8ff6-3618b7ad1e54)
-
-## Solving Algorithms and Challenges
+# 0x00. Pascal's Triangle
+```Algorithm``` ```Python```
+#### Concepts _For this project, look at these concepts:_ 
+![Technical Interview](https://www.alx-intranet.hbtn.io/concepts/100005)
