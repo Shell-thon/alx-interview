a34c22145155e021c642648de4979deb60043437
Shell-thon <shellterminal0@gmail.com>
2023-08-07 10:39:05 -0700

    pass pycodestyle on line 7 & 29

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-    """ Script that computes a minimum operations
-     needed in a CopyAll - Paste task
-    """
+"""
+Script that computes a minimum operations
+needed in a CopyAll - Paste task
+"""
+
+
 def minOperations(n):
     """
-     Method for compute the minimum number 
-     of operations for task Copy All and Paste 
-  
-     Args: 
-         n: input value 
-         factor_list: List to save the operations 
-     Return: the sum of the operations
+    Method for computing the minimum number
+    of operations for task Copy All and Paste
+    Args:
+        n: input value
+    Return: the sum of the operations
     """
     if n < 2:
         return 0
@@ -20,6 +21,12 @@ def minOperations(n):
         i += 1
         if n % i == 0:
             while n % i == 0:
-                n /= i
+                n //= i  # Use integer division
                 factor_list.append(i)
-                return sum(factor_list)
+    return sum(factor_list)  # Return outside the while loop
+
+# Example usage
+
+
+result = minOperations(12)
+print("Minimum operations:", result)
