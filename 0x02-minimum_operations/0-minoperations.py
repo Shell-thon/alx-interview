f28560258abc961149eb4f8308c72982a18bd38d
Shell-thon <shellterminal0@gmail.com>
2023-08-19 05:53:31 -0700

    updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -15,18 +15,16 @@ def minOperations(n):
     """
     if n < 2:
         return 0
-    factor_list = []
-    i = 1
-    while n != 1:
-        i += 1
+    factor_sum = 0
+    i = 2  # Start with the first prime number
+    while n > 1:
         if n % i == 0:
-            while n % i == 0:
-                n //= i  # Use integer division
-                factor_list.append(i)
-    return sum(factor_list)  # Return outside the while loop
+            factor_sum += i
+            n //= i  # Use integer division
+        else:
+            i += 1
+    return factor_sum
 
 # Example usage
-
-
 result = minOperations(12)
 print("Minimum operations:", result)
